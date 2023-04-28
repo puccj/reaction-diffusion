a0bb6b72424e3b14c999e8a4fd7b0b84174c838f
puccj <daniele.pucci2@studio.unibo.it>
2023-04-28 11:29:54 +0200

    Spelling correction

--- plot.py
+++ plot.py
@@ -4,4 +4,4 @@ import numpy as np
 data = np.loadtxt(open("dataA.dat", "rb"))
 threshold = data.mean()
 thresh = np.where(data > threshold, 255, 0)
-hetmap = sns.heatmap(thresh)
+heatmap = sns.heatmap(thresh)
